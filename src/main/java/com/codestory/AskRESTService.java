914582e1e5ffc9886c87a4f15677b6542f07a78c
ninlock <ninlock37@gmail.com>
2013-01-09 12:44:49 +0100

    update1

--- src/main/java/com/codestory/AskRESTService.java
+++ src/main/java/com/codestory/AskRESTService.java
@@ -1,12 +1,5 @@
 package com.codestory;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,13 +37,6 @@ public class AskRESTService {
         }else{
             return "Aucune réponse trouvée.";
         }
-    }
-    
-    @GET
-    @Path("/sources")
-    @Produces("text/html")
-    public String sources() throws Exception {
-       return SourceReader.getSources();
     }
     
     
---
+++ src/main/java/com/codestory/SourceRESTService.java
@@ -0,0 +1,28 @@
+package com.codestory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * JAX-RS Example
+ * 
+ * This class produces a RESTful service to read the contents of the members table.
+ */
+@Path("/sources")
+@RequestScoped
+public class SourceRESTService {
+
+    @GET
+    @Path("/")
+    @Produces("text/html")
+    public String sources() throws Exception {
+       return SourceReader.getSources();
+    }
+    
+    
+}
