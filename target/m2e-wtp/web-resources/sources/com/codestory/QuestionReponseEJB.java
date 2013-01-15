0996cec07438b9dd3b5f9dc09ccedd920317373b
YBthebest <yannickbloem@hotmail.com>
2013-01-16 00:53:11 +0100

    db local/cloud

---
+++ target/m2e-wtp/web-resources/sources/com/codestory/QuestionReponseEJB.java
@@ -0,0 +1,14 @@
+package com.codestory;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+@Local
+public interface QuestionReponseEJB {
+    
+   List<QuestionReponse> findAllQuestionReponse();
+   QuestionReponse findQuestionReponseByQuestion(String question);
+   void saveQuestionReponse(QuestionReponse questionReponse);
+   void updateQuestionReponse(QuestionReponse questionReponse);
+}
---
+++ target/m2e-wtp/web-resources/sources/com/codestory/QuestionReponseEJBBean.java
@@ -0,0 +1,37 @@
+package com.codestory;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+@Stateless
+public class QuestionReponseEJBBean implements QuestionReponseEJB {
+
+    @PersistenceContext(name="codestory")
+    EntityManager em;
+
+    public List<QuestionReponse> findAllQuestionReponse(){
+        Query query = em.createQuery("select qr from QuestionReponse qr");
+        return query.getResultList();
+    }
+
+    public QuestionReponse findQuestionReponseByQuestion(String question){
+        Query query = em.createQuery("select qr from QuestionReponse qr where question='" + question + "'");
+        QuestionReponse qr = null;
+        try {
+            qr = (QuestionReponse)query.getSingleResult();
+        } catch (Exception e) {}
+        return qr;
+    }
+
+    public void saveQuestionReponse(QuestionReponse questionReponse) {
+        em.persist(questionReponse);
+    }
+    
+    public void updateQuestionReponse(QuestionReponse questionReponse) {
+        em.merge(questionReponse);
+    }
+}
